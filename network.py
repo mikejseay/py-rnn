4cc9a895a51540a9cda3e3c6250f8add530b0b59
mikejseay <mikejseay@gmail.com>
2017-06-23 17:01:56 -0400

    a bit of reorganization. implemented harvesting innate trajectory.

--- network.py
+++ network.py
@@ -7,27 +7,26 @@
 
 import numpy as np
 from scipy.stats import norm
+from scipy.sparse import csr_matrix
 
-class Network(object):
+prng_seed = 1234
+prng = np.random.RandomState(prng_seed)
 
+
+class Generator(object):
     ''' a randomly-connected recurrent neural network '''
 
-    def __init__(self, n_units, p_plastic, p_connect, syn_strength,
-                 tau_ms, sigmoid, noise_amp):
+    def __init__(self, n_units, p_plastic, p_connect, syn_strength):
         self.n_units = n_units
         self.p_plastic = p_plastic
         self.p_connect = p_connect
         self.syn_strength = syn_strength
 
-        self.tau_ms = tau_ms
-        self.sigmoid = sigmoid
-        self.noise_amp = noise_amp
-
         self.n_plastic = int(np.round(n_units * p_plastic))
         self.scale_recurr = syn_strength / np.sqrt(p_connect * n_units)
 
-class Trial(object):
 
+class Trial(object):
     ''' a single trial and its (timing) characteristics '''
     # defaults not currently designed to be configured
     extra_train_ms = 150
@@ -53,8 +52,8 @@ class Trial(object):
         if self.plot_skip % 2 == 0:
             self.plot_skip += 1
 
-class Input(object):
 
+class Input(object):
     ''' an input to a network '''
 
     def __init__(self, trial_obj, n_units, value, start_ms, duration_ms):
@@ -71,8 +70,8 @@ class Input(object):
         input_series[0, startpulse_idx:startpulse_idx + pulsedur_samps - 1] = value
         self.series = input_series
 
-class Output(object):
 
+class Output(object):
     ''' a (desired) output from a network '''
 
     def __init__(self, trial_obj, n_units, value, center_ms, width_ms, baseline_val):
@@ -87,30 +86,93 @@ class Output(object):
         bell /= np.max(bell)  # by the way, this is a fast way to normalize a vector to 1
         self.series = bell * (value - baseline_val) + baseline_val
 
+
 class Trainer(object):
+    ''' training object.
+        consumes network, input, output, and trial objects.
+        defines some parameters relevant to training.
+        simulates a neural network and trains its weights.
+        stages are innate, recurrent, readout, and test '''
 
-    ''' training parameters '''
-
-    def __init__(self, network_obj, input_obj, output_obj, trial_obj,
+    def __init__(self, generator_obj, input_obj, output_obj, trial_obj,
+                 tau_ms, sigmoid, noise_harvest, noise_train,
                  n_trials_recurrent, n_trials_readout, n_trials_test):
 
         # other objs
-        self.network = network_obj
-        self.input = input_obj
-        self.output = output_obj
-        self.trial = trial_obj
+        self.gen = generator_obj
+        self.inp = input_obj
+        self.out = output_obj
+        self.tr = trial_obj
+
+        # training params
+        self.tau_ms = tau_ms
+        self.sigmoid = sigmoid
+        self.noise_amp = noise_train
+        self.noise_innate = noise_harvest
 
         # number of trials
         self.n_trials_recurrent = n_trials_recurrent
         self.n_trials_readout = n_trials_readout
         self.n_trials_test = n_trials_test
 
-    def train(self, stage):
-        ''' training kernel '''
+        # constant for dividing change in firing rate
+        self.time_div = tau_ms / self.tr.time_step
 
-        if stage is 'innate':
-            use_noiseamp = 0
-        else:
-            use_noiseamp = self.network.noise_amp
+    def initialize_weights(self):
 
-        
+        # generator recurrent weights (wxx)
+        wxx_mask = prng.rand(self.gen.n_units, self.gen.n_units)  # uniform distribution!
+        wxx_mask[wxx_mask <= self.gen.p_connect] = 1
+        wxx_mask[wxx_mask < 1] = 0
+        wxx_vals = prng.normal(scale=self.gen.scale_recurr,
+                               size=(self.gen.n_units, self.gen.n_units))
+        wxx_nonsparse = wxx_vals * wxx_mask
+        np.fill_diagonal(wxx_nonsparse, 0)
+        self.gen.wxx_ini = csr_matrix(wxx_nonsparse)
+
+        # input => RRN (winputx)
+        self.inp.winputx_ini = prng.normal(scale=1,
+                                           size=(self.gen.n_units, self.inp.n_units))
+
+        # RRN => output (wxout)
+        self.out.wxout_ini = prng.normal(scale=1 / np.sqrt(self.gen.n_units),
+                                         size=(self.out.n_units, self.gen.n_units))
+
+    def harvest_innate(self):
+
+        # assigning recurrent and input weights to workspace names
+        wxx = self.gen.wxx_ini
+        winputx = self.inp.winputx_ini
+
+        # creating all noise ahead of time
+        use_noiseamp = self.noise_innate
+        all_noise = np.random.normal(scale=np.sqrt(self.tr.time_step),
+                                     size=(self.gen.n_units, self.tr.n_steps))
+
+        # what we are really interested in: the innate trajectory
+        x_history = np.empty((self.gen.n_units, self.tr.n_steps))
+
+        # creating initial conditions for firing rate & activation level
+        xv = 2 * np.random.rand(self.gen.n_units, 1) - 1
+        x = self.sigmoid(xv)
+
+        for i in range(self.tr.n_steps):
+
+            in_vec = self.inp.series[:, i]
+            noise = use_noiseamp * all_noise[:, [i]]
+            xfr_new = wxx * x + winputx * in_vec + noise
+            xv += (-xv + xfr_new) / self.time_div
+            x = self.sigmoid(xv)
+
+            x_history[:, [i]] = x
+
+        self.gen.innate = x_history  # save the innate trajectory
+
+    def train_recurrent(self):
+        pass
+
+    def train_readout(self):
+        pass
+
+    def test(selfs):
+        pass
--- test_main.py
+++ test_main.py
@@ -1,27 +1,22 @@
 ''' testing the network module '''
 
-from tqdm import tqdm
-
 import numpy as np
-from scipy import sparse
-from scipy.sparse import csr_matrix, csc_matrix, coo_matrix
-from scipy.stats import norm
 
 import network as N
 
 NETWORK_PARAMS = {'n_units': 800,
-                 'p_plastic': 0.6,
-                 'p_connect': 0.1,
-                 'syn_strength': 1.5,
-                 'tau_ms': 10,
-                 'sigmoid': np.tanh,
-                 'noise_amp': 0.001}
+                  'p_plastic': 0.6,
+                  'p_connect': 0.1,
+                  'syn_strength': 1.5,
+                  'tau_ms': 10,
+                  'sigmoid': np.tanh,
+                  'noise_amp': 0.001}
 
 TRIAL_PARAMS = {'length_ms': 1000,
                 'spacing': 2,
                 'time_step': 1,
                 'start_train_ms': 250,
-                'end_train_ms': 1400,}
+                'end_train_ms': 1400, }
 
 INPUT_PARAMS = {'n_units': 1,
                 'value': 5,
@@ -30,78 +25,16 @@ INPUT_PARAMS = {'n_units': 1,
 
 OUTPUT_PARAMS = {'n_units': 1,
                  'value': 1,
-                'center_ms': 1250,
-                'width_ms': 30,
-                'baseline_val': 0.2}
+                 'center_ms': 1250,
+                 'width_ms': 30,
+                 'baseline_val': 0.2}
 
 TRAIN_PARAMS = {'n_trials_recurrent': 20,
                 'n_trials_readout': 10,
                 'n_trials_test': 10}
 
 NET = N.Network(**NETWORK_PARAMS)
-TRYAL = N.Trial(**TRIAL_PARAMS)
-IN = N.Input(TRYAL, **INPUT_PARAMS)
-OUT = N.Output(TRYAL, **OUTPUT_PARAMS)
-TRAIN = N.Trainer(NET, IN, OUT, TRYAL, **TRAIN_PARAMS)
-
-WXX_mask = np.random.rand(NET.n_units, NET.n_units)  # uniform distribution!
-WXX_mask[WXX_mask <= NET.p_connect] = 1
-WXX_mask[WXX_mask < 1] = 0
-WXX_vals = np.random.normal(scale=NET.scale_recurr, size=(NET.n_units, NET.n_units))
-WXX_nonsparse = WXX_vals * WXX_mask
-np.fill_diagonal(WXX_nonsparse, 0)
-
-WXX = csr_matrix(WXX_nonsparse)
-WInputX = np.random.normal(scale=1, size=(NET.n_units, IN.n_units))
-WXOut = np.random.normal(scale=1/np.sqrt(NET.n_units), size=(OUT.n_units, NET.n_units))
-
-
-WXX_ini = WXX.copy()
-WXOut_ini = WXOut.copy()
-
-X_history = np.zeros((NET.n_units, TRYAL.n_steps))
-Out_history = np.zeros((OUT.n_units, TRYAL.n_steps))
-WXOut_len = np.zeros((TRYAL.n_steps))
-WXX_len = np.zeros((TRYAL.n_steps))
-dW_readout_len = np.zeros((TRYAL.n_steps))
-dW_recurr_len = np.zeros((TRYAL.n_steps))
-
-Xv = 2 * np.random.rand(NET.n_units, 1) - 1
-X = NET.sigmoid(Xv)
-O = np.zeros((OUT.n_units,1))
-
-TRAIN_RECURR = False
-TRAIN_READOUT = False
-train_window = 0
-
-use_noiseamp = 0
-time_div = NET.tau_ms / TRYAL.time_step
-
-for i in tqdm(range(TRYAL.n_steps)):
-
-    in_vec = IN.series[:, i]
-    noise = use_noiseamp * np.random.normal(scale=np.sqrt(TRYAL.time_step), size=(NET.n_units,1))
-    Xv_current = WXX * X + WInputX * in_vec + noise
-    Xv += (-Xv + Xv_current) / time_div
-    X = NET.sigmoid(Xv)
-    O = np.dot(WXOut, X)
-
-    if (i == TRYAL.start_train_n):
-        train_window = True
-    if (i == TRYAL.end_train_n):
-        train_window = False
-
-    if train_window and i % TRYAL.spacing == 0:
-
-        if TRAIN_RECURR:
-            error = X - Target_innate_X[:, i]
-            for plas in 1:NET.n_plastic
-
-        if TRAIN_READOUT:
-            pass
-        
-    Out_history[:, i] = O
-    X_history[:, [i]] = X
-    WXOut_len[i] = np.sqrt(np.sum(np.square(WXOut[:])))
-    WXX_len[i] = np.sqrt(np.sum(np.square(WXX[:])))
-
+TRIAL = N.Trial(**TRIAL_PARAMS)
+IN = N.Input(TRIAL, **INPUT_PARAMS)
+OUT = N.Output(TRIAL, **OUTPUT_PARAMS)
+TRAIN = N.Trainer(NET, IN, OUT, TRIAL, **TRAIN_PARAMS)
